d5b128bbee7dc033bf9cd6c48c20fdb974b2a1b1
Dina <yanbulatova.dk@yandex.ru>
2022-09-17 12:14:56 +0300

    Написана программа

--- Program.cs
+++ Program.cs
@@ -4,3 +4,27 @@
 //лучше обойтись исключительно массивами.
 
 
+string[] array1 = new string[5] {"HELLO", "89", "la-la-la", "GeekBrains", "^-^"};
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
